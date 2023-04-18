9b7ee43bb7ef3b08c276667f80f63b997f27b0e9
Franco Silva <franco_tigres18@hotmail.com>
2023-04-18 17:24:43 -0600

    Auth Layout estilos add

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -3,9 +3,12 @@ import { Outlet } from "react-router-dom"
 const AuthLayout = () => {
   return (
     <>
-      <div>AuthLayout</div>
+      <main className="container mx-auto mt-5 md:mt-20 p-5 md:justify-center">
 
-      <Outlet />
+        <div className="md:w-2/3 lg:1/2">
+          <Outlet />
+        </div>
+      </main>
     </>
   )
 }
