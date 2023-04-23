b39204495e8af7f4ecf2786f4a7563092b587dc7
Franco Silva <franco_tigres18@hotmail.com>
2023-04-23 15:40:50 -0600

    Funcionalidad Registrar add

--- src/pages/Registrar.jsx
+++ src/pages/Registrar.jsx
@@ -1,13 +1,82 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
+import Alerta from "../components/Alerta"
+import clienteAxios from "../config/clienteAxios"
 
 const Registrar = () => {
+
+  const [nombre, setNombre] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [repetirPassword, setRepetirPassword] = useState('')
+
+  const [alerta, setAlerta] = useState({})
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+
+    if ([nombre, email, password, repetirPassword].includes('')) {
+      setAlerta({
+        msg: 'Todos los campos son Obligatorios',
+        error: true
+      })
+      return
+    }
+
+    if (password !== repetirPassword) {
+      setAlerta({
+        msg: 'Las contraseñas no son iguales',
+        error: true
+      })
+      return
+    }
+
+    if (password.length < 6) {
+      setAlerta({
+        msg: 'La contraseña es muy corta, agrega minimo 6 caracteres',
+        error: true
+      })
+      return
+    }
+
+    setAlerta({})
+
+    // Crear el usuario en la API
+    try {
+      const { data } = await clienteAxios.post(`/usuarios`, { nombre, email, password })
+
+      setAlerta({
+        msg: data.msg,
+        error: false
+      })
+
+      setNombre('')
+      setEmail('')
+      setPassword('')
+      setRepetirPassword('')
+
+    } catch (error) {
+      setAlerta({
+        msg: error.response.data.msg,
+        error: true
+      })
+    }
+  }
+
+  const { msg } = alerta
+
   return (
     <>
       <h1 className="text-sky-600 font-black text-5xl capitalize">Crea tu cuenta y administra tus
         <span className="text-slate-700"> proyectos</span>
       </h1>
 
-      <form action="" className="my-10 bg-white shadow rounded-lg px-10 py-5">
+      {msg && <Alerta alerta={alerta} />}
+
+      <form
+        className="my-10 bg-white shadow rounded-lg px-10 py-5"
+        onSubmit={handleSubmit}
+      >
         <div className="my-5 ">
           <label htmlFor="nombre" className="uppercase text-gray-600 block text-xl font-bold">Nombre:</label>
           <input
@@ -15,6 +84,8 @@ const Registrar = () => {
             type="text"
             placeholder="Tu nombre"
             className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+            value={nombre}
+            onChange={e => setNombre(e.target.value)}
           />
         </div>
 
@@ -25,6 +96,8 @@ const Registrar = () => {
             type="email"
             placeholder="Email de registro"
             className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
           />
         </div>
 
@@ -35,6 +108,8 @@ const Registrar = () => {
             type="password"
             placeholder="Contraseña de registro"
             className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
           />
         </div>
 
@@ -45,6 +120,8 @@ const Registrar = () => {
             type="password"
             placeholder="Repite tu contraseña"
             className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+            value={repetirPassword}
+            onChange={e => setRepetirPassword(e.target.value)}
           />
         </div>
 
