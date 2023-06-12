a36fcbef4114a8ee898d77dbe1a5fde9f4d1cde5
Franco Silva <franco_tigres18@hotmail.com>
2023-06-12 10:31:00 -0600

    Provider Proyecto - Funcionalidad editar add

--- src/context/ProyectosProvider.jsx
+++ src/context/ProyectosProvider.jsx
@@ -11,6 +11,7 @@ const ProyectosProvider = ({ children }) => {
   const [proyecto, setProyecto] = useState({});
   const [cargando, setCargando] = useState(false)
   const [modalFormularioTarea, setModalFormularioTarea] = useState(false)
+  const [tarea, setTarea] = useState({})
 
   const navigate = useNavigate()
 
@@ -176,11 +177,46 @@ const ProyectosProvider = ({ children }) => {
     }
   }
 
+  const editarTarea = async (tarea) => {
+    try {
+      const token = localStorage.getItem('token')
+      if (!token) return
+
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`
+        }
+      }
+
+      const { data } = await clienteAxios.put(`/tareas/${tarea.id}`, tarea, config)
+
+      const proyectoActualizado = { ...proyecto }
+      proyectoActualizado.tareas = proyectoActualizado.tareas.map(tareaState => tareaState._id === data._id ? data : tareaState)
+      setProyecto(proyectoActualizado)
+
+      setAlerta({})
+      setModalFormularioTarea(false)
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   const handleModalTarea = () => {
     setModalFormularioTarea(!modalFormularioTarea)
+    setTarea({})
   }
 
   const submitTarea = async (tarea) => {
+    if (tarea?.id) {
+      await editarTarea(tarea)
+    } else {
+      await crearTarea(tarea)
+    }
+  }
+
+  const crearTarea = async (tarea) => {
     try {
       const token = localStorage.getItem('token')
       if (!token) return
@@ -207,6 +243,10 @@ const ProyectosProvider = ({ children }) => {
     }
   }
 
+  const handleModalEditarTarea = (tarea) => {
+    setTarea(tarea)
+    setModalFormularioTarea(true)
+  }
   return (
     <ProyectosContext.Provider
       value={{
@@ -220,7 +260,9 @@ const ProyectosProvider = ({ children }) => {
         eliminarProyecto,
         modalFormularioTarea,
         handleModalTarea,
-        submitTarea
+        submitTarea,
+        handleModalEditarTarea,
+        tarea
       }}
     >{children}</ProyectosContext.Provider>
   )
