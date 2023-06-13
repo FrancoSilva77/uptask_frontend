5279e397373c3bb5d3dc8b2f91c7bd178d6ae8a6
Franco Silva <franco_tigres18@hotmail.com>
2023-06-13 08:43:12 -0600

    Mostrar Modal en Tarea add

--- src/components/Tarea.jsx
+++ src/components/Tarea.jsx
@@ -2,7 +2,7 @@ import { formatearFecha } from "../utils/formatearFecha"
 import useProyectos from "../hooks/useProyectos"
 
 const Tarea = ({ tarea }) => {
-  const { handleModalEditarTarea } = useProyectos()
+  const { handleModalEditarTarea, handleModalEliminarTarea } = useProyectos()
   const { descripcion, nombre, prioridad, fechaEntrega, _id, estado } = tarea
   return (
     <div className="border-b p-5 flex justify-between items-center">
@@ -31,6 +31,7 @@ const Tarea = ({ tarea }) => {
 
         <button
           className="bg-red-600 px-4 py-3 text-white uppercase font-bold text-sm rounded-lg"
+          onClick={() => handleModalEliminarTarea(tarea)}
         >Eliminar</button>
       </div>
     </div>
